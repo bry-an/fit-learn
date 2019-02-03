f5a0a12f78991252cb1955ee1879387e7e96bcb0
bry-an <bbyunis@gmail.com>
2019-02-02 17:15:35 -0700

    add vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { Component, Vue } from "vue-property-decorator";
+import About from "@/views/About.vue";
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: { About }
 })
 export default class Home extends Vue {}
 </script>
